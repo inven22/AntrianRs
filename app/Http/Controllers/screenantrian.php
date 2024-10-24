156fea84535c166d886638f60ba1f37363ae08ae
Brahmantya Santoso <brahmantyasantoso5@gmail.com>
2024-10-24 10:25:41 +0700

    integrasi api

--- app/Http/Controllers/screenantrian.php
+++ app/Http/Controllers/screenantrian.php
@@ -9,36 +9,30 @@ use Illuminate\Support\Facades\Log;
 class screenantrian extends Controller
 {
     public function index(){
-        // Membuat client Guzzle untuk melakukan request ke API Odoo
         $client = new Client();
 
-        // URL API Odoo untuk antrian umum dan antrian jaminan
         $antrianUmumUrl = 'http://localhost:8069/api/antrian_umum';
         $antrianJaminanUrl = 'http://localhost:8069/api/antrian_jaminan';
 
         try {
-            // Mengirim request GET ke API Odoo untuk antrian umum
             $responseUmum = $client->get($antrianUmumUrl, [
                 'headers' => [
                     'Accept' => 'application/json',
                     'Content-Type' => 'application/json',
-                    // Tambahkan token jika API Odoo butuh autentikasi
-                    // 'Authorization' => 'Bearer YOUR_API_TOKEN'
+                   
                 ],
             ]);
 
-            // Logging response status dan URL untuk debugging
             Log::info('Request ke API antrian umum berhasil', [
                 'url' => $antrianUmumUrl,
                 'status_code' => $responseUmum->getStatusCode()
             ]);
 
-            // Mengirim request GET ke API Odoo untuk antrian jaminan
             $responseJaminan = $client->get($antrianJaminanUrl, [
                 'headers' => [
                     'Accept' => 'application/json',
                     'Content-Type' => 'application/json',
-                    // 'Authorization' => 'Bearer YOUR_API_TOKEN'
+                    
                 ],
             ]);
 
@@ -47,17 +41,14 @@ class screenantrian extends Controller
                 'status_code' => $responseJaminan->getStatusCode()
             ]);
 
-            // Parsing response dari API ke array
             $dataUmum = json_decode($responseUmum->getBody()->getContents(), true);
             $dataJaminan = json_decode($responseJaminan->getBody()->getContents(), true);
 
-            // Mengambil data antrian yang dibutuhkan
             $antrianUmum = $dataUmum['data'][0]['no_appointment'] ?? 'N/A';
             $antrianJaminan = $dataJaminan['data'][0]['no_appointment'] ?? 'N/A';
             $poliUmum = $dataUmum['data'][0]['poli'] ?? 'N/A';
             $poliJaminan = $dataJaminan['data'][0]['poli'] ?? 'N/A';
 
-            // Logging ID antrian
             Log::info('Data antrian umum', [
                 'no_appointment' => $antrianUmum,
                 'poli' => $poliUmum,
@@ -69,7 +60,6 @@ class screenantrian extends Controller
             ]);
 
         } catch (\Exception $e) {
-            // Log error jika ada masalah dengan request
             Log::error('Terjadi kesalahan saat mengambil data antrian', [
                 'message' => $e->getMessage(),
             ]);
@@ -81,7 +71,7 @@ class screenantrian extends Controller
                 'poliJaminan' => 'Error',
             ]);
         }
-        
+
         return view('antrian.baris', [
             'antrianUmum' => $antrianUmum,
             'antrianJaminan' => $antrianJaminan,
